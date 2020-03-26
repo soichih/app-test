3fb4b7271e56da9c3b34e982b2b91b121814f561
Soichi Hayashi <hayashis@iu.edu>
2020-03-26 14:48:11 +0000

    added test outputs

--- test.py
+++ test.py
@@ -4,18 +4,29 @@
 #PBS -l walltime=00:00:10
 
 import os
+import json
+import shutil
 
 #import dipy.tracking as dipytracking
 #dipytracking.bench()
 
-try:
-    os.stat("output")
-except:
-    os.mkdir("output")
+with open('config.json') as f:
+  config = json.load(f)
 
-f = open("output/test.txt", "w")
-f.write("yo")
-f.close()
+try:
+    os.stat("output_1")
+except:
+    os.mkdir("output_1")
+shutil.copyfile(config['t1'], "output_1/t1.nii.gz")
+
+try:
+    os.stat("output_2")
+except:
+    os.mkdir("output_2")
+shutil.copyfile(config['t1'], "output_2/bad.nii.gz")
+
+#for deprecated output
+shutil.copyfile(config['t1'], "out.nii.gz")
 
 #f = open("product.json", "w")
 #f.write("{\"tags\": [\"from_app\"], \"brainlife\": [{\"type\": \"success\", \"msg\": \"here is message\"}]}")
